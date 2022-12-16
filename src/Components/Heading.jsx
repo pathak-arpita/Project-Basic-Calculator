e5b15e4fa7976f8f31d3035a4ac60d8a8596fae6
Arpita <pathak.arpita6497@gmail.com>
2022-12-17 02:24:45 +0530

    latest push

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,12 @@
+import './Heading.css'
+const Heading = () => {
+    return (
+        <>
+          <br></br>
+            <div className="div">
+                <span className="hover-text"><b>BASIC CALCULATOR</b></span>
+            </div>
+        </>
+    )
+}
+export default Heading;
